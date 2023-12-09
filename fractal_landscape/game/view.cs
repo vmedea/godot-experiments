c9bcfadef41d169f6ca35e0d4fb5a8bdfac0f527
mara <vmedea@protonmail.com>
2023-12-09 14:04:39 +0100

    add landscape viewer
    as an experiment with Godot and C#, port over Captain Blood fractal landscape viewer
    (see http://bringerp.free.fr/RE/CaptainBlood/main.php5))

---
+++ fractal_landscape/game/view.cs
@@ -0,0 +1,14 @@
+  public class View {
+	public int firstSegmentIndex_x4;
+	public int lastSegmentIndex_x4;
+	public int distanceToFirstSegment;
+	public int distanceToLastSegment;
+	public int speed;
+	public long heading;
+	public int cursorX;
+	public int cursorY;
+	public int heightFixed;
+	public int heightGoal;
+	public int unkDefense;
+	public int unk2C;
+  }
---
+++ fractal_landscape/game_texture.cs
@@ -0,0 +1,120 @@
+using Godot;
+using System;
+
+public partial class game_texture : TextureRect
+{
+	const int WIDTH = 256;
+	const int HEIGHT = 128;
+	
+	Image img = Image.Create(WIDTH, HEIGHT, false, Image.Format.L8);
+	byte [] buffer = new byte [WIDTH * HEIGHT];
+	Game _game;
+	double _frame;
+		
+	[Signal]
+	public delegate void InfoEventHandler(string debug, string distance, string heading, string speed);
+	
+	public game_texture()
+	{
+		_game = new Game(false);
+		_frame = 0.0;
+		_game.fullRaytracer = true;
+	}
+	
+	public override void _Ready()
+	{
+		img.Fill(Color.Color8(255, 0, 255, 255));
+		//GD.Print("Hello from C# ", img.GetWidth(), " ", img.GetHeight(), " ", Color.Color8(255, 0, 255, 255));
+		ImageTexture t = this.Texture as ImageTexture;
+		t.SetImage(img);
+	}
+	
+	void UpdateTexture() {
+		bool blue = true;
+		if (_game.imageRaytracer && !_game.blueRaytracer)
+			blue = false;
+
+		// XXX show both?
+		// would need to factor this out to a GameModel class that provides two textures, or make one big buffer, of say, 256x256
+		if (blue) {
+			// Two interpeaved bitplanes, 256x128
+			const int planes = 2;
+			const int stride = WIDTH / 8 * planes;
+			int ptr = 0;
+			for (int y=0; y < HEIGHT; ++y) {
+				for (int x=0; x < WIDTH; ++x) {
+					int in_ofs = y * stride + (x >> 4) * 2 * planes + ((x >> 3) & 1);
+					int in_bit = 7 - (x & 0x7);
+					int idx = ((_game.viewGfx[in_ofs + 0] >> (in_bit)) & 1) | 
+							  (((_game.viewGfx[in_ofs + 2] >> (in_bit)) & 1) << 1);
+					buffer[ptr] = (byte)idx;
+					ptr += 1;
+				}
+			}
+		} else {
+			// Four interleaved bitplanes, 320x200
+			const int xofs = 32;
+			const int yofs = 18;
+			const int planes = 4;
+			const int stride = 320 / 8 * planes;
+			int ptr = 0;
+			for (int y=yofs; y < (yofs + HEIGHT); ++y) {
+				for (int x=xofs; x < (xofs + WIDTH); ++x) {
+					int in_ofs = y * stride + (x >> 4) * 2 * planes + ((x >> 3) & 1);
+					int in_bit = 7 - (x & 0x7);
+					int idx = ((_game.screen[in_ofs + 0] >> (in_bit)) & 1) | 
+							  (((_game.screen[in_ofs + 2] >> (in_bit)) & 1) << 1) |
+							  (((_game.screen[in_ofs + 4] >> (in_bit)) & 1) << 2) |
+							  (((_game.screen[in_ofs + 6] >> (in_bit)) & 1) << 3);
+					buffer[ptr] = (byte) idx;
+					ptr += 1;
+				}
+			}
+		}
+
+		img.SetData(WIDTH, HEIGHT, false, Image.Format.L8, buffer);
+		ImageTexture t = this.Texture as ImageTexture;
+		t.Update(img);
+	}
+	
+	public void Iterate() {
+		if (_game.Flying_finishLineReachedUnk [0] != -3) {
+			_game.Iterate();
+			UpdateTexture();
+		}
+		
+		// Information for HUD
+		string debug = _game.debugString;
+		string distance = "";
+		string heading = "";
+		string speed = "";
+		
+		if (_game.Flying_finishLineReachedUnk[0] >= 0) {
+			if (_game.Flying_finishLineReachedUnk[0] == 0)
+				distance = string.Format("Distance to start of canyon: {0} ", 0x8000 - _game.Flying_distanceDone);
+			else
+				distance = string.Format("Distance to end of canyon: {0} ", _game.Flying_finishLineReachedUnk [0]);
+			heading = string.Format("Heading: {0:+0.000;-0.000;0.000}^ ({1:+0.000;-0.000;0.000})", _game.view.heading / 65536.0f, (_game.headingDeltaFixed >> 8) / 256.0f);
+			speed = string.Format ("Speed: {0} ", _game.view.speed);
+		}
+		EmitSignal(SignalName.Info, debug, distance, heading, speed);
+	}
+
+	public override void _Process(double delta)
+	{
+		_frame -= delta;
+		if (_frame < 0) {
+			Iterate();
+			_frame += 0.030;
+		}
+	}
+
+	public override void _Input(InputEvent evt)
+	{
+		if (evt is InputEventKey kev) {
+			GD.Print("kev", kev);
+		} else if (evt is InputEventMouseMotion mev) {
+			GD.Print("mev ", mev.Relative, " ", mev.ButtonMask);
+		}
+	}
+}
