04598106585f1a82808b42d00797aed801c4aa63
mara <vmedea@protonmail.com>
2023-12-09 16:16:23 +0100

    speed bar

--- fractal_landscape/root.gd
+++ fractal_landscape/root.gd
@@ -4,12 +4,16 @@ extends Node2D
 #   nicer speed bar (gradient bar)
 
 @onready var info_label: Label = $TextureRect/InfoLabel;
+@onready var speed_gauge: TextureProgressBar = $TextureRect/Speed;
 
 func _set_info(debug: String, distance: String, heading: String, speed: int):
-	info_label.text = debug + "\n" + distance + "\n" + heading + ("\nSpeed: %d" % [speed]);
+	info_label.text = debug + "\n" + distance + "\n" + heading + "\nSpeed: ";
+	speed_gauge.value = speed;
+
 
 func _ready():
 	$TextureRect.Info.connect(_set_info)
+
 
 func _input(evt):
 	if evt.is_action_pressed("ui_cancel"):
