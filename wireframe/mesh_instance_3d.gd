75a1334cc7c7a0c5b774278d33588f1b65fed880
mara <vmedea@protonmail.com>
2024-11-07 08:39:19 +0000

    wireframe: simplify
    use flags per vertex instead of full colors

--- wireframe/mesh_instance_3d.gd
+++ wireframe/mesh_instance_3d.gd
@@ -11,31 +11,20 @@ func _ready() -> void:
 	vertices.push_back(Vector3(-1, 1, 0))
 	vertices.push_back(Vector3(1, 1, 0))
 	
+	var flags := PackedByteArray()
+	flags.append_array(PackedByteArray([255, 255, 0, 0]))
+	flags.append_array(PackedByteArray([255, 0,   0, 0]))
+	flags.append_array(PackedByteArray([0,   255, 0, 0]))
 	
-	var color0 := PackedByteArray()
-	color0.append_array(PackedByteArray([255, 0, 0, 255]))
-	color0.append_array(PackedByteArray([0, 255, 0, 255]))
-	color0.append_array(PackedByteArray([0, 0, 0, 255]))
-	
-	color0.append_array(PackedByteArray([255, 0, 0, 255]))
-	color0.append_array(PackedByteArray([0, 0, 0, 255]))
-	color0.append_array(PackedByteArray([0, 0, 255, 255]))
-	
-	var color1 := PackedByteArray()
-	color1.append_array(PackedByteArray([255, 255, 0, 255]))
-	color1.append_array(PackedByteArray([0, 0, 0, 255]))
-	color1.append_array(PackedByteArray([255, 0, 255, 255]))
-	
-	color1.append_array(PackedByteArray([0, 0, 0, 255]))
-	color1.append_array(PackedByteArray([0, 255, 255, 255]))
-	color1.append_array(PackedByteArray([255, 0, 255, 255]))
+	flags.append_array(PackedByteArray([255, 0,   0, 0]))
+	flags.append_array(PackedByteArray([0,   255, 0, 0]))
+	flags.append_array(PackedByteArray([255, 255, 0, 0]))
 	
 	var arr_mesh := ArrayMesh.new()
 	var arrays = []
 	arrays.resize(Mesh.ARRAY_MAX)
 	arrays[Mesh.ARRAY_VERTEX] = vertices
-	arrays[Mesh.ARRAY_CUSTOM0] = color0
-	arrays[Mesh.ARRAY_CUSTOM1] = color1
+	arrays[Mesh.ARRAY_CUSTOM0] = flags
 	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
 	
 	var material := ShaderMaterial.new()
