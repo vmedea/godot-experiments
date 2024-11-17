3672b5d23cdab59f9867bf0ee28d85e198cf653e
mara <vmedea@protonmail.com>
2024-11-17 20:27:11 +0100

    use post-import script

---
+++ wireframe/postprocess_wireframe.gd
@@ -0,0 +1,30 @@
+@tool
+extends EditorScenePostImport
+
+const OUT_PATH: String = "res://wireframes/"
+
+func _post_import(scene: Node) -> Object:
+	var children := scene.get_children()
+	print('Updating meshes to wireframe ', scene.name)
+	
+	for child in children:
+		if not is_instance_of(child, MeshInstance3D):
+			continue
+
+		var mesh_in: ArrayMesh = child.mesh
+
+		assert(mesh_in.surface_get_primitive_type(0) == Mesh.PRIMITIVE_TRIANGLES)
+		var mesh_out := MeshUtils.unroll_vertices(mesh_in, [Mesh.ARRAY_VERTEX, Mesh.ARRAY_CUSTOM0])
+		assert(mesh_out.surface_get_primitive_type(0) == Mesh.PRIMITIVE_TRIANGLES)
+		
+		var material := ShaderMaterial.new()
+		material.shader = preload("res://shader.gdshader")
+		mesh_out.surface_set_material(0, material)
+		
+		#child.mesh = mesh_out
+		#child.set_surface_override_material(0, material)
+		#print('Updated ', child)
+		var filename_out := OUT_PATH + scene.name + ".res"
+		ResourceSaver.save(mesh_out, filename_out)
+
+	return scene # Return the modified root node when you're done.
